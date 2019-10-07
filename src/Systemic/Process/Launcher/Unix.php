6aa857be7f4bd11d8fd75df4d8c2801d08d05dfc
Tom Wright <tom@inflatablecookie.com>
2019-10-07 16:03:51 +0100

    Bypass script if input generator
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Systemic/Process/Launcher/Unix.php
+++ src/Systemic/Process/Launcher/Unix.php
@@ -206,7 +206,7 @@ class Unix extends Base
             $command = 'sudo -u '.$this->user.' '.$command;
         }
 
-        if (Systemic::$os->which('script')) {
+        if (!$this->inputGenerator && Systemic::$os->which('script')) {
             if (Systemic::$os->isMac()) {
                 $command = 'script -q /dev/null '.$command;
             } else {
