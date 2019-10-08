d3ed431ba14b136eb5326363c7e27f127d1bba12
Tom Wright <tom@inflatablecookie.com>
2019-10-08 01:28:10 +0100

    Updated linux launcher
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/Systemic/Process/Launcher/Unix.php
+++ src/Systemic/Process/Launcher/Unix.php
@@ -209,7 +209,7 @@ class Unix extends Base
         if ($this->decoratable && Systemic::$os->which('script')) {
             if (Systemic::$os->isMac()) {
                 $command = 'script -q /dev/null '.$command;
-            } elseif (!$this->user) {
+            } else {
                 $command = 'script -e -q -c "'.$command.'" /dev/null';
             }
         }
